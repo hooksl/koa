f6f635784006b89f2562d58f9ee3fb47ed7599da
lightning <lightning004@outlook.com>
2024-04-19 07:42:26 +0800

    引用数据库环境变量

---
+++ post-data.sh
@@ -0,0 +1,1 @@
+curl -X POST -d '{ "id":"3", "provinceId":"3"}' -H "Content-Type: application/json" http://127.0.0.1:8001/users/register
--- src/db/seq.js
+++ src/db/seq.js
@@ -1,14 +1,20 @@
 const { Sequelize } = require('sequelize')
+const { MYSQL_DB, MYSQL_USER, MYSQL_PWD, MYSQL_HOST } = require('./../config/config')
+
 /**
  * database
  * username
  * password
  */
-const seq = new Sequelize('', 'root', '123456', {
-    host: 'localhost',
-    // 数据库类型
-    dialect: 'mysql'
-})
+const seq = new Sequelize(
+    MYSQL_DB,
+    MYSQL_USER,
+    MYSQL_PWD,
+    {
+        host: MYSQL_HOST,
+        // 数据库类型
+        dialect: 'mysql'
+    })
 // 返回值是一个promise对象
 seq.authenticate().then(() => {
     console.log('数据库连接成功')
