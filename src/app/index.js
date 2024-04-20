a2921085598db5a537bbb5226bcccd1e924949dc
lightning <lightning004@outlook.com>
2024-04-21 01:52:14 +0800

    错误信息监听

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,12 @@
 const Koa = require('koa')
 const userRouter = require('../router/user')
 const { koaBody } = require('koa-body')
+const errHandler = require('./errHandler')
 const app = new Koa()
 
 // 在注册路由前注册
 app.use(koaBody())
 // 必须是一个函数
 app.use(userRouter.routes())
-app.on('error', (err, ctx) => {
-    console.log(err)
-})
+app.on('error', errHandler)
 module.exports = app
--- src/middleware/user.js
+++ src/middleware/user.js
@@ -6,7 +6,7 @@ const userValidator = async (ctx, next) => {
     if (!user_name || !password) {
         console.log('用户名或密码为空', ctx.request.body)
         // 如果用户名或密码为空 就返        // 封装了错误信息 ctx.app.emit在提示错误信息时使用
-        ctx.app.emit('error', userFormateError)
+        ctx.app.emit('error', userFormateError, ctx)
         return
     }
     // 否则放行向下执行
@@ -18,7 +18,7 @@ const verifyUser = async (ctx, next) => {
     // 合理性
     const { user_name } = ctx.request.body
     if (await getUserInfo({ user_name })) {
-        ctx.app.emit('error', userAlreadyExisted)
+        ctx.app.emit('error', userAlreadyExisted, ctx)
         return
     }
     await next()
